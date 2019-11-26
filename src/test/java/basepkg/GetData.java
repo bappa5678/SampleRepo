e85bf7db710553ad007169ab0e5aa67a31371566
bappa5678 <bappa.5687@gmail.com>
2019-11-26 18:07:10 +0530

    upload

--- src/test/java/basepkg/GetData.java
+++ src/test/java/basepkg/GetData.java
@@ -1,12 +1,24 @@
 package basepkg;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GetData {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception 
 
 	{
-	System.out.println("This is a test line");
+		   System.setProperty("webdriver.chrome.driver", "C:\\Users\\28693\\Desktop\\EclipseWorkSpace\\chromedriver_win32\\chromedriver.exe"); 
+		WebDriver driver = new ChromeDriver();
+		   driver.manage().window().maximize();  
+		    driver.get("http://www.google.com/");
+		    
+		    driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("Selenium");
+		    driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]")).click();
+		    Thread.sleep(5000);	
+		    driver.quit();
+	        System.out.println("This is a test line");
 		
 	}
 
