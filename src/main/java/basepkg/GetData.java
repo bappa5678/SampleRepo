ca9611101acc2af6acb23b990556ec3cd4c8243a
bappa5678 <bappa.5687@gmail.com>
2019-11-27 17:49:13 +0530

    update

--- src/main/java/basepkg/GetData.java
+++ src/main/java/basepkg/GetData.java
@@ -1,11 +1,16 @@
 package basepkg;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.commons.io.FileUtils;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -44,7 +49,7 @@ public class GetData {
 	    driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("Selenium  abdjhdsgksjhgk");
 		   driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]")).click();
 		  Thread.sleep(5000);
-		  
+		   takeSnapShot(driver, "C:\\Users\\28693\\Desktop\\snapshot\\test.jpeg") ;  
 		  
 		  System.out.println("After browser Launch");
 		    driver.quit();
@@ -58,5 +63,28 @@ public class GetData {
 		}
 		
 	}
+	
+	
+	 public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+
+	        //Convert web driver object to TakeScreenshot
+
+	        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+
+	        //Call getScreenshotAs method to create image file
+
+	                File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+	            //Move image file to new destination
+
+	                File DestFile=new File(fileWithPath);
+
+	                //Copy file at destination
+
+	                FileUtils.copyFile(SrcFile, DestFile);
+
+	    }
+	
+	
 
 }
