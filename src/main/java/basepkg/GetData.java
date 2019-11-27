19edf5c6939e15c4ac7c63f4d26c860eac400976
bappa5678 <bappa.5687@gmail.com>
2019-11-27 17:32:35 +0530

    update

--- src/main/java/basepkg/GetData.java
+++ src/main/java/basepkg/GetData.java
@@ -1,8 +1,11 @@
 package basepkg;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -19,14 +22,24 @@ public class GetData {
 		
 		System.out.println("This is before webdriver");  
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\28693\\Desktop\\EclipseWorkSpace\\chromedriver_win32\\chromedriver.exe"); 
-		
-		
 		WebDriver driver = new ChromeDriver();
 		
 		try {
 		
 		   driver.manage().window().maximize();  
 		    driver.get("http://www.google.com/");
+		    		   
+		    WebDriverWait wait = new WebDriverWait(driver,60);
+		       wait.until(new ExpectedCondition<Boolean>(){
+		           @Override
+		           public Boolean apply(WebDriver d){
+		               return ((JavascriptExecutor)d).executeScript("return document.readyState").equals("complete");
+		           }
+		       });
+		    
+		    
+		    
+		    
 		    
 	    driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("Selenium  abdjhdsgksjhgk");
 		   driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]")).click();
